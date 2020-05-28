5c50fda84c17b54bf951c9992e3c7ad1d092351c
Dawid Szczepanik <szczepan0801@gmail.com>
2020-05-28 17:07:04 +0200

    1

---
+++ Arcade/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+using namespace sf;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
